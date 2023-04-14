8621813326ebebd7d93f27d1012ce778bd672a29
JarrryGuillaume <guillaume.jarry5@gmail.com>
2023-04-14 13:17:37 +0200

    ok commit

---
+++ backend/utils/config.py
@@ -0,0 +1,9 @@
+class Config:
+    openai_api_key = "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"
+    huggingface_hub_api_key = "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"
+    pinecone_api_key = "8fdad3f2-158b-474a-813d-372dc9d218ce"
+    TEMP_FOLDER = 'temp'
+    SID_DEFAULT = 'default'
+    AWS_ACCESS_KEY_ID = "AKIAXX6HNL4ZSGVN6A4C"
+    AWS_SECRET_ACCESS_KEY = "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"
+    AWS_REGION_NAME = "eu-west-3"
